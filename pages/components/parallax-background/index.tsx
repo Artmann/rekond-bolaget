9d2d29bd4830243b6b55ddb77869673f3bb85783
Christoffer Artmann <Artgaard@gmail.com>
2020-02-10 21:08:02 +0100

    fix: Don't use state for background opacity.

--- pages/components/parallax-background/index.tsx
+++ pages/components/parallax-background/index.tsx
@@ -1,10 +1,9 @@
-import React, { ReactElement, useState, useLayoutEffect } from 'react';
+import React, { ReactElement, useState, useLayoutEffect, useRef, MutableRefObject, RefObject } from 'react';
 import styled from 'styled-components';
 
 interface ParallaxBackgroundProps {}
 interface BackgroundProps {
   index: number;
-  opacity: number;
   url: string;
 }
 interface ContainerProps {
@@ -19,7 +18,7 @@ const Background = styled.div<BackgroundProps>`
   background-size: cover;
   bottom: 0;
   left: 0;
-  opacity: ${ props => props.opacity};
+  opacity: 1;
   position: absolute;
   right: 0;
   top: 0;
@@ -33,18 +32,6 @@ const Container = styled.div<ContainerProps>`
   top: 0;
   z-index: -200;
 `;
-
-function debounce(interval: number, fn: Function): Function {
-  let handle: number;
-
-  return function(): void {
-    clearTimeout(handle);
-
-    handle = setTimeout(() => {
-      fn();
-    }, interval);
-  }
-}
 
 function getMaxScroll(): number {
   return Math.max(
@@ -66,19 +53,31 @@ function ease(t: number): number {
   return Math.max(Math.min(value, 1), 0)
 }
 
-export default function ParallaxBackground({}: ParallaxBackgroundProps): ReactElement {
+function updateOpacity(scrollProgression: number, index: number, ref: MutableRefObject<HTMLDivElement>, length: number): void {
+  const stepSize = 1 / length;
+  const shouldRender = scrollProgression >= index * stepSize;
+  const relativeProgression = Math.max(Math.min(scrollProgression - index * stepSize, 1), 0);
+  const step = relativeProgression / stepSize;
+  const value = index === 0 ? 1 : ease(step);
+  const opacity = shouldRender ? Math.round(value * 100) / 100 : 0;
 
+  ref.current.style.opacity = opacity.toString();
+}
+
+export default function ParallaxBackground({}: ParallaxBackgroundProps): ReactElement {
+  const backgroundRefs = backgrounds.map(bg => useRef<HTMLDivElement>(null));
   const [ height, setHeight ] = useState('100vh');
-  const [ scrollProgression, setScrollProgression ] = useState(0);
 
   useLayoutEffect(() => {
     const onScrollHandler = (): void => {
-      debounce(150, () => {
-        const maxScroll = getMaxScroll();
+      const maxScroll = getMaxScroll();
+      const scrollProgression = maxScroll > 0  && window.scrollY > 100 ? window.scrollY / maxScroll : 0;
 
-        setHeight(`${ window.innerHeight }px`);
-        setScrollProgression(maxScroll > 0  && window.scrollY > 100 ? window.scrollY / maxScroll : 0);
-      })();
+      for (let i = 0; i < backgroundRefs.length; i++) {
+        requestAnimationFrame(() => {
+          updateOpacity(scrollProgression, i, backgroundRefs[i], backgrounds.length);
+        });
+      }
     };
 
     window.addEventListener('scroll', onScrollHandler);
@@ -93,14 +92,12 @@ export default function ParallaxBackground({}: ParallaxBackgroundProps): ReactEl
   return (
     <Container height={ height } >
       { backgrounds.map((url: string, index: number) => {
-          const stepSize = 1 / backgrounds.length;
-          const shouldRender = scrollProgression >= index * stepSize;
-          const relativeProgression = Math.max(Math.min(scrollProgression - index * stepSize, 1), 0);
-          const step = relativeProgression / stepSize;
-          const value = index === 0 ? 1 : ease(step);
-          const opacity = shouldRender ? value : 0;
-
-          return <Background index={ index } key={ index } opacity={ opacity } url={ url } />;
+          return <Background
+            index={ index }
+            key={ index }
+            ref={ backgroundRefs[index] }
+            url={ url }
+            />;
         })
       }
     </Container>
